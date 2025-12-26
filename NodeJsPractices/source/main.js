db07c1a650cbca7699cee67c0f24e932a8fe7a25
alfinarif <cleavercode@gmail.com>
2025-12-26 22:17:31 +0300

    work with server and url

--- NodeJsPractices/source/main.js
+++ NodeJsPractices/source/main.js
@@ -1,10 +1,22 @@
-var http = require('http');
+const http = require('http');
 
-// created server
-var server = http.createServer((req, res)=>{
-    res.end("Hello Bangladesh");
+// created nodeJs server
+const server = http.createServer((req, res)=>{
+    if(req.url == '/'){
+        res.writeHead(200);
+        res.write('<h1>This is Home Page.!</h1>');
+        res.end();
+    }else if(req.url == '/about'){
+        res.writeHead(200, {'Content-type': 'text/html'});
+        res.write('<h1>This is About Page</h1>');
+        res.end();
+    }else{
+        res.writeHead(500, {'Content-type': 'text/html'});
+        res.write("<h1 style='color:red'>Internal Server Errors!</h1>");
+        res.end();
+    }
 });
 
-// define port for server
+// define server port using .listen method
 server.listen(5050);
-console.log("Server running...");
+console.log("Server run successfully");
