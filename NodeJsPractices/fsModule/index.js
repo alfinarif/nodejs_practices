0e056efac59ea62c1ef03404f1e9701c2a5ccde2
alfinarif <cleavercode@gmail.com>
2025-12-27 20:04:19 +0300

    work with Asynchronous and Synchronous

--- NodeJsPractices/fsModule/index.js
+++ NodeJsPractices/fsModule/index.js
@@ -7,12 +7,118 @@ const fs = require('fs');
 const server = http.createServer((req, res) =>{
 
     if (req.url == '/'){
-        // Asynchronous ======================
-        fs.readFile('home.html', (error, data)=>{
-            res.writeHead(200, {'Content-type': 'text/html'});
-            res.write(data);
+        // Asynchronous for readFile ======================
+
+        // fs.readFile('home.html', (error)=>{
+        //     res.writeHead(200, {'Content-type': 'text/html'});
+        //     res.write(data);
+        //     res.end();
+        // });
+
+
+        // Synchronous for readFile ============
+
+        // let data = fs.readFileSync('home.html');
+        // res.writeHead(200, {'Content-type': 'text/html'});
+        // res.write(data);
+        // res.end();
+
+
+
+        // Asynchronous for writeFile =====================
+
+        // fs.writeFile('demo.txt', 'Hello world', (error)=>{
+        //     if (error){
+        //         res.writeHead(200, {'Content-type': 'text/html'});
+        //         res.write('File write failed...');
+        //         res.end();
+        //     }else {
+        //         res.writeHead(200, {'Content-type': 'text/html'});
+        //         res.write('File write success');
+        //         res.end();
+        //     }
+        // });
+
+
+        // Synchronous for writeFile ======================
+
+        // let error = fs.writeFileSync('demoSync.txt', 'Welcome to nodeJs server');
+        // if(error){
+        //     res.writeHead(500, {'Content-type': 'text/html'});
+        //     res.write('File write failed...');
+        //     res.end();
+        // }else {
+        //     res.writeHead(200, {'Content-type': 'text/html'});
+        //     res.write('File write success');
+        //     res.end();
+        // }
+
+
+        // Asynchronous for fs.rename =====================
+
+        // fs.rename('demo.txt', 'demoAsync.txt', (error)=>{
+        //     if (error){
+        //         res.writeHead(500, {'Content-type': 'text/html'});
+        //         res.write('File rename failed...');
+        //         res.end();
+        //     }else{
+        //         res.writeHead(200, {'Content-type': 'text/html'});
+        //         res.write('File renamed success');
+        //         res.end();
+        //     }
+        // })
+
+
+
+
+        // Synchronous for fs.rename =====================
+
+        // let error = fs.renameSync('demo.txt', 'demoSync.txt');
+        // if (error){
+        //     res.writeHead(500, {'Content-type': 'text/html'});
+        //     res.write('File rename failed...');
+        //     res.end();
+        // }else{
+        //     res.writeHead(200, {'Content-type': 'text/html'});
+        //     res.write('File renamed success');
+        //     res.end();
+        // }
+
+
+
+        // Asynchronous for unlink (delete) file ============
+
+        // fs.unlink('demoAsync.txt', (error)=>{
+        //     if (error){
+        //         res.writeHead(500, {'Content-type': 'text/html'});
+        //         res.write('File delete failed...');
+        //         res.end();
+        //     }else{
+        //         res.writeHead(200, {'Content-type': 'text/html'});
+        //         res.write('File deleted success');
+        //         res.end();
+        //     }
+        // })
+
+
+
+        // Synchronous for unlink (delete) file =============
+
+        let error = fs.unlinkSync('demoSync.txt');
+        if (error){
+            res.writeHead(500, {'Content-type': 'text/html'});
+            res.write('File delete failed...');
             res.end();
-        });
+        }else{
+            res.writeHead(200, {'Content-type': 'text/html'});
+            res.write('File deleted success');
+            res.end();
+        }
+
+
+
+
+
     }
 
 });
