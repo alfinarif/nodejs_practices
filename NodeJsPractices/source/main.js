63784c2209100dea3e9376522966105383da44b9
alfinarif <cleavercode@gmail.com>
2025-12-27 17:55:57 +0300

    created package.json file

--- NodeJsPractices/source/main.js
+++ NodeJsPractices/source/main.js
@@ -1,22 +1,25 @@
 const http = require('http');
 
-// created nodeJs server
-const server = http.createServer((req, res)=>{
-    if(req.url == '/'){
-        res.writeHead(200);
-        res.write('<h1>This is Home Page.!</h1>');
-        res.end();
-    }else if(req.url == '/about'){
+const server = http.createServer(( req, res ) =>{
+    if (req.url == '/'){
         res.writeHead(200, {'Content-type': 'text/html'});
-        res.write('<h1>This is About Page</h1>');
+        res.write("<h1>This is Home Page</h1>");
         res.end();
-    }else{
+    }else if (req.url == '/about'){
+        res.writeHead(200, {'Content-type': 'text/html'});
+        res.write("<h1>This is About Page</h1>");
+        res.end();
+    }else if (req.url == '/contact'){
+        res.writeHead(200, {'Content-type': 'text/html'});
+        res.write("<h1>This is Contact Page</h1>");
+        res.end();
+    }else {
         res.writeHead(500, {'Content-type': 'text/html'});
-        res.write("<h1 style='color:red'>Internal Server Errors!</h1>");
+        res.write("<h1 style='color: red'>Internal Server Error..</h1>");
         res.end();
     }
 });
 
-// define server port using .listen method
+
 server.listen(5050);
-console.log("Server run successfully");
+console.log('Server Connected Successfully');
