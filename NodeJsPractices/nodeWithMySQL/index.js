6edc16ca82cbdd7c3ab309f1379e3219f5453b76
alfinarif <cleavercode@gmail.com>
2025-12-29 22:12:55 +0300

    deleteDataFromDatabase

--- NodeJsPractices/nodeWithMySQL/index.js
+++ NodeJsPractices/nodeWithMySQL/index.js
@@ -1,5 +1,6 @@
 let mySQL = require('mysql');
 
+// database config
 let DbConnectionConfig = {
     host: "localhost",
     user: "root",
@@ -7,6 +8,7 @@ let DbConnectionConfig = {
     database: "school"
 }
 
+// database connection
 let DbConnect = mySQL.createConnection(DbConnectionConfig);
 
 DbConnect.connect((error)=>{
@@ -14,19 +16,32 @@ DbConnect.connect((error)=>{
         console.log("Connection Failed");
     }else{
         console.log("Connected Successfully");
-        InsertData(DbConnect);
+        // InsertData(DbConnect);
+        DeleteData(DbConnect);
     }
 });
 
 
-// insert function
-const InsertData = (DbConnect)=>{
+// insert data function
+let InsertData = (DbConnect)=>{
     let SqlQuery = "INSERT INTO `students_list`(`name`, `roll`, `class`, `phone`, `email`, `city`) VALUES ('Mohammad Shohan', '02', 'two', '0506897109', 'shohan@gmail.com', 'Cumilla')"
     DbConnect.query(SqlQuery, (error)=>{
         if(error){
             console.log("Data insert failed");
         }else{
             console.log("Data inserted success");
+        }
+    })
+}
+
+// delete data function
+let DeleteData = (DbConnect)=>{
+    let SqlQuery = "DELETE FROM `students_list` WHERE id=7";
+    DbConnect.query(SqlQuery, (error)=>{
+        if(error){
+            console.log("Data delete failed");
+        }else{
+            console.log("Data deleted success");
         }
     })
 }
