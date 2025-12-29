224b9592fe427de360bae8b8ae36422fd5a5a0bf
alfinarif <cleavercode@gmail.com>
2025-12-29 22:01:10 +0300

    insertDataIntoDatabase

--- NodeJsPractices/nodeWithMySQL/index.js
+++ NodeJsPractices/nodeWithMySQL/index.js
@@ -1,17 +1,32 @@
-var mySQL = require('mysql');
+let mySQL = require('mysql');
 
-var DbConnectionConfig = {
+let DbConnectionConfig = {
     host: "localhost",
     user: "root",
-    password: ""
+    password: "",
+    database: "school"
 }
 
-var DbConnect = mySQL.createConnection(DbConnectionConfig);
+let DbConnect = mySQL.createConnection(DbConnectionConfig);
 
 DbConnect.connect((error)=>{
     if(error){
         console.log("Connection Failed");
     }else{
         console.log("Connected Successfully");
+        InsertData(DbConnect);
     }
-})
+});
+
+
+// insert function
+const InsertData = (DbConnect)=>{
+    let SqlQuery = "INSERT INTO `students_list`(`name`, `roll`, `class`, `phone`, `email`, `city`) VALUES ('Mohammad Shohan', '02', 'two', '0506897109', 'shohan@gmail.com', 'Cumilla')"
+    DbConnect.query(SqlQuery, (error)=>{
+        if(error){
+            console.log("Data insert failed");
+        }else{
+            console.log("Data inserted success");
+        }
+    })
+}
