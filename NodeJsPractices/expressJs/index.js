5fa27139563e6b865bfbad2b148c107f158e197e
alfinarif <cleavercode@gmail.com>
2026-01-03 20:13:16 +0300

    expressJs

--- NodeJsPractices/expressJs/index.js
+++ NodeJsPractices/expressJs/index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express()
 
 
+app.get('/', (req, res)=>{
+    res.send("Hello world");
+    console.log("Work fine");
+})
+
+
 app.listen(8080, (error)=>{
     if(error){
         console.log("server connection errors");
     }else{
         console.log("Server is running");
     }
-})
+});
