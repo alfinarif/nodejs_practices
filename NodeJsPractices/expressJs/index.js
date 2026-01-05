6abe1062d925a5e221004a85d65fe2749290d980
Alfin Arif <62806376+alfinarif@users.noreply.github.com>
2026-01-05 19:35:03 +0300

    practiceOnMobile

--- NodeJsPractices/expressJs/index.js
+++ NodeJsPractices/expressJs/index.js
@@ -12,6 +12,10 @@ app.get('/about', (req, res)=>{
     res.send("Hello about page");
 })
 
+app.get('/contact', (req, res)=>{
+    res.send("Hello contact page");
+})
+
 
 app.listen(8080, (error)=>{
     if(error){
