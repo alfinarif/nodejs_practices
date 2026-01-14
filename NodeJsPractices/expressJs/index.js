64475f9fc95e3f4d5894087c631c4fd86596cd6a
alfinarif <cleavercode@gmail.com>
2026-01-14 19:10:31 +0300

    getPostPutDeleteRequest

--- NodeJsPractices/expressJs/index.js
+++ NodeJsPractices/expressJs/index.js
@@ -1,23 +1,31 @@
-const express = require('express');
-const app = express()
+const exress = require('express');
+
+const app = exress();
 
 
 app.get('/', (req, res)=>{
-    res.send("Hello world");
-    console.log("Work fine");
+    res.send('Hello world');
 });
 
 
-app.get('/about', (req, res)=>{
+app.post('/about', (req, res)=>{
     res.send("Hello about page");
 })
 
+app.put('/contact', (req, res)=>{
+    res.send("Hello contact page");
+})
 
 
-app.listen(8080, (error)=>{
+app.delete('/service', (req, res)=>{
+    res.send("Hello service page");
+})
+
+
+app.listen(5050, (error)=>{
     if(error){
-        console.log("server connection errors");
+        console.log('Server Failed To Run...!');
     }else{
-        console.log("Server is running");
+        console.log('Server Run Successfully');
     }
-});
+})
