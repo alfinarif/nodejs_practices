d50e371aefc640bfae1384c09ce8f7de56f923a0
alfinarif <cleavercode@gmail.com>
2026-01-16 15:58:35 +0300

    routeLevelMiddleware

--- NodeJsPractices/expressJs/middleware.js
+++ NodeJsPractices/expressJs/middleware.js
@@ -4,23 +4,33 @@ const express = require('express');
 const app = express();
 
 // application level middleware
-app.use((req, res, next)=>{
+// app.use((req, res, next)=>{
+//
+//     console.log('This is application level middleware');
+//     next();
+// });
 
-    console.log('This is application level middleware');
+
+// created route level middleware for about path
+app.use('/about', (req, res, next)=>{
+    console.log('This middleware use for only about page');
     next();
-});
-
-
-
+})
 
 
 app.get('/', (req, res)=>{
     res.send('this is home page');
 });
 
+
+
+
 app.get('/about', (req, res)=>{
     res.send('this is about page');
 });
+
+
+
 
 app.get('/contact', (req, res)=>{
     res.send('this is contact page');
