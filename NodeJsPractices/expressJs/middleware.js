8901ac23fccbe86b09e1bf8063899ad35ebc9055
alfinarif <cleavercode@gmail.com>
2026-01-16 15:51:56 +0300

    applicationLevelMiddleware

---
+++ NodeJsPractices/expressJs/middleware.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+
+const app = express();
+
+// application level middleware
+app.use((req, res, next)=>{
+
+    console.log('This is application level middleware');
+    next();
+});
+
+
+
+
+
+app.get('/', (req, res)=>{
+    res.send('this is home page');
+});
+
+app.get('/about', (req, res)=>{
+    res.send('this is about page');
+});
+
+app.get('/contact', (req, res)=>{
+    res.send('this is contact page');
+});
+
+
+
+app.listen(5050, (error)=>{
+    if(error){
+        console.log('server run faild');
+    }
+    else {
+        console.log('server run success');
+    }
+})
