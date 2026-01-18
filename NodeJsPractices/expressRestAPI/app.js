5c1b138783c727450fe70563f50a3cc093a21afe
alfinarif <cleavercode@gmail.com>
2026-01-18 21:06:05 +0300

    undefinedRoute

--- NodeJsPractices/expressRestAPI/app.js
+++ NodeJsPractices/expressRestAPI/app.js
@@ -8,4 +8,10 @@ app.use("/api/v1", router);
 
 
 
+// Undefined Routes here '/*splat' use for undefine route in express v5
+app.use('/*splat', (req, res)=>{
+    res.status(404).json({status: "fail", data: "Not Fount"});
+});
+
+
 module.exports = app;
