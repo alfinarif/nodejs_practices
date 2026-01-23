3445f9da6c61afe8b451fb8e778a920639f21637
alfinarif <cleavercode@gmail.com>
2026-01-23 16:20:07 +0300

    defaultValue&versionKey

--- NodeJsPractices/expressRestAPI/src/models/studentsModel.js
+++ NodeJsPractices/expressRestAPI/src/models/studentsModel.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const dataSchema = mongoose.Schema({
-    Name: String,
-    Roll: String,
-    Class: String,
-    Remarks: String
-});
+    Name: {type: String},
+    Roll: {type: Number},
+    Class: {type: String},
+    Remarks: {type: String, default: "No Remarks"}
+
+}, {versionKey: false});
 
 const dataSchemaModels = mongoose.model('students', dataSchema);
 
