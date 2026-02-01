b82b6eb623a011b427f31007fb2fdda287dcf890
alfinarif <cleavercode@gmail.com>
2026-02-01 20:21:05 +0300

    reactJs

--- ReactJsPractices/reactJsPractice_1/vite-project/src/App.jsx
+++ ReactJsPractices/reactJsPractice_1/vite-project/src/App.jsx
@@ -1,24 +1,17 @@
 
 const App = ()=>{
 
-    let marks = 75;
+    let item = ['A', 'B', 'C', 'D'];
 
     return (
         <div>
-            {(()=>{
-                if(marks >80 && marks<100){
-                    return <h1>A+</h1>
+            <select>
+                {
+                    item.map((item, i)=>{
+                        return <option key={i.toString()}>{item}</option>
+                    })
                 }
-                else if(marks>70 && marks <80){
-                    return <h1>A</h1>
-                }
-                else if(marks>60 && marks<70){
-                    return <h1>A-</h1>
-                }
-                else {
-                    return <h1>F</h1>
-                }
-            })()}
+            </select>
         </div>
     );
 
