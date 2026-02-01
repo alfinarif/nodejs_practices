1f9e55b63d856e058b9c72ba08055313958ea586
alfinarif <cleavercode@gmail.com>
2026-02-01 21:04:51 +0300

    reactJs

--- ReactJsPractices/reactJsPractice_1/vite-project/src/App.jsx
+++ ReactJsPractices/reactJsPractice_1/vite-project/src/App.jsx
@@ -4,14 +4,16 @@
 const App = ()=>{
     let status = false;
 
-    switch (status) {
-        case true:
-            return <button>Logout</button>
-        case false:
-            return <button>Login</button>
-        default:
-            return <h1>Nothing returns</h1>
-    }
+    return (
+        <div>
+            {
+                status == true?
+                    <button>Logout</button>
+                :
+                    <button>Login</button>
+            }
+        </div>
+    )
 
 };
 
