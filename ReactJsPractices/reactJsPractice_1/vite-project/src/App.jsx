1d3abdea939ecfe128d6c7140e9e2ab6eb7f327c
alfinarif <cleavercode@gmail.com>
2026-02-01 20:15:06 +0300

    reactJs

--- ReactJsPractices/reactJsPractice_1/vite-project/src/App.jsx
+++ ReactJsPractices/reactJsPractice_1/vite-project/src/App.jsx
@@ -1,15 +1,24 @@
 
 const App = ()=>{
 
-    let marks = 81;
+    let marks = 75;
 
     return (
         <div>
-            {
-                marks > 80?<h1>Brilliant Result</h1>
-                : 
-                <h1>Avarage Result</h1>
-            }
+            {(()=>{
+                if(marks >80 && marks<100){
+                    return <h1>A+</h1>
+                }
+                else if(marks>70 && marks <80){
+                    return <h1>A</h1>
+                }
+                else if(marks>60 && marks<70){
+                    return <h1>A-</h1>
+                }
+                else {
+                    return <h1>F</h1>
+                }
+            })()}
         </div>
     );
 
