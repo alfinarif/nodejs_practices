87121aabf35b14311efa9dd7c668963b884f9681
alfinarif <cleavercode@gmail.com>
2026-02-08 19:36:11 +0300

    eventHandling

--- ReactJsPractices/practiceProject_2/src/components/ClockComponent4.jsx
+++ ReactJsPractices/practiceProject_2/src/components/ClockComponent4.jsx
@@ -1,14 +1,43 @@
 import { Component } from 'react';
 
 class ClockComponent4 extends Component {
+    state = {date: new Date(), locale: 'bn-BD'};
+
+    tick(){
+        this.setState({
+            date: new Date()
+        })
+    }
+
+    componentDidMount(){
+        this.clockTimer = setInterval(()=>{
+            this.tick();
+        })
+    }
+
+    componentWillUnmount(){
+        clearInterval(()=>{
+            this.clockTimer;
+        })
+    }
+
+    handleClick = ()=>{
+        this.setState({
+            locale: 'en-US',
+        });
+
+    }
+
+
     render() {
         return (
             <div>
                 <h1>
                     <span>
-                        {new Date().toLocaleTimeString()}
+                        {this.state.date.toLocaleTimeString(this.state.locale)}
                     </span>
                 </h1>
+                <button onClick={this.handleClick}>Click Here</button>
             </div>
         );
     }
