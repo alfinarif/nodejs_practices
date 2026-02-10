01e103e0535c837b62a4d9d6f7152805b1bba85d
alfinarif <cleavercode@gmail.com>
2026-02-10 20:20:39 +0300

    formHandling

--- ReactJsPractices/practiceProject_2/src/components/FormPractice.jsx
+++ ReactJsPractices/practiceProject_2/src/components/FormPractice.jsx
@@ -3,20 +3,43 @@ import { Component } from 'react';
 class FormPractice extends Component {
 
     state = {
-        title: "JavaScript"
+        title: "input",
+        textarea: "textarea",
+        library: "reactJs",
     }
 
     changeHandler = (e)=>{
         //e.preventDefault();
-        console.log(e.target.value)
+        //console.log(e.target.value)
 
-        this.setState({
-            title: e.target.value
-        })
+        if(e.target.type === "text"){
+            this.setState({
+            title: e.target.value,
+
+            });
+
+        } else if(e.target.type === "textarea") {
+            this.setState({
+            textarea: e.target.value,
+
+            })
+        }else if(e.target.type === "select-one") {
+            this.setState({
+            library: e.target.value,
+
+            })
+        } else {
+            console.log('Nothing here!')
+        }
+
+        
     }
 
     render() {
-        const {title} = this.state;
+        const {title, textarea, library} = this.state;
+        console.log(title)
+        console.log(textarea)
+        console.log(library)
         return (
             <div>
                 <form>
@@ -26,7 +49,30 @@ class FormPractice extends Component {
                         value={title} 
                         onChange={this.changeHandler} 
                     />
-                    <p>{title}</p>
+
+                    <br/>
+                    <br/>
+
+                    <textarea 
+                        name="textarea" 
+                        value={textarea} 
+                        onChange={this.changeHandler} >
+                    </textarea>
+
+                    <br/>
+                    <br/>
+                    
+                    <select value={library} onChange={this.changeHandler}>
+
+                        <option value="reactJs">ReactJs</option>
+                        <option value="angular">Angular</option>
+
+                    </select>
+
+                    <br/>
+                    <br/>
+
+                    <input type="checkbox"/>
                 </form>
                 
             </div>
