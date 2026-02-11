b68d1ff3923fa3f02726f1202f050b1ba4432b0d
alfinarif <cleavercode@gmail.com>
2026-02-11 19:05:41 +0300

    liftingStateUp

---
+++ ReactJsPractices/practiceProject_2/src/components/Calculator.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Calculator extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Calculator;
