68083e08d65f814a15cfe00d9f02280b7e2c1983
alfinarif <cleavercode@gmail.com>
2026-02-11 20:12:01 +0300

    liftingStateUp

--- ReactJsPractices/practiceProject_2/src/components/Calculator.jsx
+++ ReactJsPractices/practiceProject_2/src/components/Calculator.jsx
@@ -2,14 +2,34 @@ import { Component } from 'react';
 import TemparetureInput from './TemparetureInput';
 
 class Calculator extends Component {
+    state = {
+        temperature: "",
+        scale: "c",
+    };
+
+    handleChange = (e, scale)=>{
+        this.setState({
+            temperature: e.target.value,
+            scale,
+        })
+    };
 
     render() {
+
+        const {temperature, scale} = this.state;
+
         return (
             <div>
                 
-                <TemparetureInput scale="c"  />
+                <TemparetureInput 
+                    scale="c"  
+                />
+
                 <br/>
-                <TemparetureInput scale="f"  />
+
+                <TemparetureInput 
+                    scale="f"  
+                />
 
             </div>
         );
--- ReactJsPractices/practiceProject_2/src/components/TemparetureInput.jsx
+++ ReactJsPractices/practiceProject_2/src/components/TemparetureInput.jsx
@@ -1,31 +1,24 @@
-import { Component } from 'react';
 
 const scaleNames = {
     c: "Celsius",
     f: "Fahrenheit",
 }
-class TemparetureInput extends Component {
-    state = {
-        temperature: "",
-    }
 
-    temparetureChange = (e)=>{
-        this.setState({
-            temperature: e.target.value,
-        })
-    }
-    render() {
-        const {temperature} = this.state;
-        const {scale} = this.props;
+
+const TemparetureInput = (scale, temperature, onTamperatureChange) => {
+
         return (
 
-                <fieldset>
-                    <legend>Enter temperature in {scaleNames[scale]}: </legend>
-                    <input type="text" value={temperature} onChange={this.temparetureChange} />
-                </fieldset>
+            <fieldset>
+                <legend>Enter temperature in {scaleNames[scale]}: </legend>
+                <input 
+                    type="text" 
+                    value={temperature} 
+                    onChange={onTamperatureChange} 
+                />
+            </fieldset>
 
         );
-    }
-}
+};
 
 export default TemparetureInput;
