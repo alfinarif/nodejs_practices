33a64f281fe1048f55f1abebccbbb3bb356678a7
alfinarif <cleavercode@gmail.com>
2026-02-11 18:51:27 +0300

    formControll

--- ReactJsPractices/practiceProject_2/src/components/FormPractice3.jsx
+++ ReactJsPractices/practiceProject_2/src/components/FormPractice3.jsx
@@ -1,11 +1,63 @@
 import { Component } from 'react';
 
 class FormPractice3 extends Component {
+    state = {
+        email: "",
+        password: "",
+    }
+
+    formControllHandler = (e)=>{
+        if(e.target.type === "email"){
+            this.setState({
+                email: e.target.value,
+            });
+        } else if(e.target.type === "password"){
+            this.setState({
+                password: e.target.value,
+            })
+        } else{
+            console.log('Nothing here!')
+        }
+    };
+
+
+    formSubmitHandler = (e)=>{
+        e.preventDefault();
+        const {email, password} = this.state;
+        console.log(email, password)
+    }
+
+
+
     render() {
+
+        const {email, password} = this.state;
+
         return (
             <div>
-                <form>
-                    <input  />
+                <form onSubmit={this.formSubmitHandler}>
+                    <input
+                        name="email"
+                        type="email"
+                        placeholder="Example@gmail.com"
+                        value={email}
+                        onChange={this.formControllHandler}
+                    />
+                    <br/>
+                    <br/>
+                    <input
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={this.formControllHandler}
+                    />
+                    <br/>
+                    <br/>
+                    <input
+                        type="submit"
+                        value="Login"
+                    />
                 </form>
             </div>
         );
