a595b749ecccd87b114839b1c7a8614bfdfa10ba
alfinarif <cleavercode@gmail.com>
2026-02-13 20:59:20 +0300

    crudapp

--- MernProjects/crudProject_1/server/app.js
+++ MernProjects/crudProject_1/server/app.js
@@ -1,4 +1,30 @@
 const router = require('./src/routes/api');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
 
 
 
+
+
+
+// database
+URI = "mongodb://127.0.0.1:27017/crudapp";
+OPTION = {user: "", pass: ""};
+mongoose.connect(URI, OPTION)
+    .then(()=>{
+        console.log('Database connected successfully');
+    })
+    .catch((err)=>{
+        console.log('There is something wrong');
+    })
+
+
+app.use(router);
+
+
+module.exports = app;
---
+++ MernProjects/crudProject_1/server/index.js
@@ -0,0 +1,35 @@
+const app = require('./app');
+const dotenv = require('dotenv');
+
+dotenv.config({path: 'config.env'});
+
+
+
+
+
+app.listen(process.env.RUNNING_PORT, (error)=>{
+    if(error){
+        console.log('there is something wrong');
+    } else {
+        console.log('server is running...!');
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
